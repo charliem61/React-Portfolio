fa299a98b9076d5ac3462e4e1ee032edbdd8b099
Mason Walker <masonwalker30@gmail.com>
2022-04-18 00:12:29 -0700

    added more card info

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -5,7 +5,9 @@ function Portfolio(){
         <div>
             Portfolio 
                 <PortfolioCard title="BitPage" body="bit page body"/>
-                <PortfolioCard title=""/>
+                <PortfolioCard title="TutorTea" body="tutortea body"/>
+                <PortfolioCard title="Social Network API- FindFace" body="findface body"/>
+
         </div>
     )
 }
