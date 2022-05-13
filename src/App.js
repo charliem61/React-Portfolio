4ab10e4e07cf87ce08046023de0de86b17f93c5c
Mason Walker <masonwalker30@gmail.com>
2022-05-13 13:19:34 -0700

    debug3

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ function App() {
   }
   return (
     <div className="App" style={{ backgroundImage: `url(${background})` }}>
-      sdaffasd
     <header>
       <Navigation renderPage={renderPage} setRenderPage={setRenderPage} />
     </header>
